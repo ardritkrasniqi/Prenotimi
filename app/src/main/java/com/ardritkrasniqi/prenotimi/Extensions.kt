b05350a41d4affd285f29179610eb544a255caeb
AcerE5 <ardritk1@gmail.com>
2020-02-21 16:05:24 +0100

    armagedon

---
+++ app/src/main/java/com/ardritkrasniqi/prenotimi/Extensions.kt
@@ -0,0 +1,17 @@
+package com.ardritkrasniqi.prenotimi
+
+import org.threeten.bp.DayOfWeek
+import org.threeten.bp.temporal.WeekFields
+import java.util.*
+
+fun daysOfWeekFromLocale(): Array<DayOfWeek> {
+    val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+    var daysOfWeek = DayOfWeek.values()
+    // Order `daysOfWeek` array so that firstDayOfWeek is at index 0.
+    if (firstDayOfWeek != DayOfWeek.MONDAY) {
+        val rhs = daysOfWeek.sliceArray(firstDayOfWeek.ordinal..daysOfWeek.indices.last)
+        val lhs = daysOfWeek.sliceArray(0 until firstDayOfWeek.ordinal)
+        daysOfWeek = rhs + lhs
+    }
+    return daysOfWeek
+}
